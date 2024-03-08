f3f8ff1807eab40b395d9255f0f7798cd1fc288f
JLDURON <JLDURON@Andress-MacBook-Pro.local>
2024-03-08 14:29:17 -0600

    new file called sets.py (currently learning)

--- books/python-bible/booleans.py
+++ books/python-bible/booleans.py
@@ -1,5 +1,6 @@
 a = True
 b = False
 
+# yes I totally knew how to make booleans in python (trust me, im a genius)
 print(type(a))
 print(type(b))
---
+++ books/python-bible/sets.py
@@ -0,0 +1,4 @@
+# what is a set? a set in mathematics is a organized collections of objects and can be represented in either set-builder form or roster form. In Python, sets are similar to lists, but they contain only unique elements. Python has a built-in method to create sets: set()
+set1 = set()
+# printing the set
+print(type(set1))
