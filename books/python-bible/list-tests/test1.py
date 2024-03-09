44b6a97ee00bd220308a18b46d76f26087a774bb
JLDURON <JLDURON@Andress-MacBook-Pro.local>
2024-03-08 19:00:14 -0600

    new test dir and files in it ofc

---
+++ books/python-bible/list-tests/test1.py
@@ -0,0 +1,37 @@
+# List
+helloList = ['1','2','3','4','5','6','7','8','9','10']
+
+print('enter the amount of friends you want me to have from 1 - 10: \n')
+
+numPick = int(input(" 1 \n 2 \n 3 \n 4 \n 5 \n 6 \n 7 \n 8 \n 9 \n 10 \n"))
+
+# if statements
+if numPick == 1:
+    print(f"I have {helloList[-10]} friends plus -{helloList[-10]} friends, which is zero")
+
+if numPick == 2:
+    print(f"I have {helloList[-9]} friends plus -{helloList[-9]} friends, which is zero")
+
+if numPick == 3:
+    print(f"I have {helloList[-8]} friends plus -{helloList[-8]} friends, which is zero")
+
+if numPick == 4:
+    print(f"I have {helloList[-7]} friends plus -{helloList[-7]} friends, which is zero")
+
+if numPick == 5:
+    print(f"I have {helloList[-6]} friends plus -{helloList[-6]} friends, which is zero")
+
+if numPick == 6:
+    print(f"I have {helloList[-5]} friends plus -{helloList[-5]} friends, which is zero")
+
+if numPick == 7:
+    print(f"I have {helloList[-4]} friends plus -{helloList[-4]} friends, which is zero")
+
+if numPick == 8:
+    print(f"I have {helloList[-3]} friends plus -{helloList[-3]} friends, which is zero")
+
+if numPick == 9:
+    print(f"I have {helloList[-2]} friends plus -{helloList[-2]} friends, which is zero")
+
+if numPick == 10:
+    print(f"I have {helloList[-1]} friends plus -{helloList[-1]} friends, which is zero")
